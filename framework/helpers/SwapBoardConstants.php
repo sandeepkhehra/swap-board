1f015f4a15d7adbb907beb004b5ef0d430f94b96
sandeepkhehra <sandeep.khehra243@gmail.com>
2018-12-07 19:11:57 +0530

    Initial base commit

---
+++ framework/helpers/SwapBoardConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('ABSPATH') or die('Not permitted!');
+
+abstract class SwapBoardConstants
+{
+	const PLUGIN_LONG_NAME = 'Swap Board';
+	const PLUGIN_SHORT_NAME = 'Swap';
+	const PLUGIN_SLUG = 'sboard';
+	const PLUGIN_PAGE_TITLE = self::PLUGIN_LONG_NAME . ' &mdash; ';
+}
---
+++ swap-board.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Swap Board
+ * Description: The best swap board application.
+ * Version: 1.0
+ * Author: Sagmetic Infotech Pvt. Ltd.
+ * Author URI: https://sagmetic.com
+ * License: MIT
+ */
+
+require_once 'vendor' . DIRECTORY_SEPARATOR . 'bootstrap.php';
